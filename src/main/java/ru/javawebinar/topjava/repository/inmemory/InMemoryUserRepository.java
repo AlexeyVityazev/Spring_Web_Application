1fa4b580f0a371d18e197acbc2f79e9ce1d23ace
AlexeyVityazev1987 <alexeyvityazev@gmail.com>
2023-02-14 16:34:03 +0300

    commit #2 HW_2

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,11 +6,10 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
@@ -28,7 +27,13 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            return user;
+        }
+        // handle case: update, but not present in storage
+        return repository.computeIfPresent(user.getId(), (id, oldMeal) -> user);
     }
 
     @Override
@@ -40,12 +45,21 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        List<User> sortedList = new ArrayList<>();
+        repository.entrySet().stream().sorted(Map.Entry.comparingByValue((object1, object2) -> object1.getName().compareTo(object2.getName())))
+                .forEachOrdered(x -> sortedList.add(x.getValue()));
+
+        return sortedList;
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        List<User> list = new ArrayList<>();
+        repository.entrySet()
+                .stream().filter(x -> x.getValue().getEmail() == email)
+                .forEachOrdered(x -> list.add(x.getValue()));
+
+        return list.get(0);
     }
 }
